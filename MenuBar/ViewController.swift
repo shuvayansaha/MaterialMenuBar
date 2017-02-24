e989ba12d0ae58dcc99edfed27034312ebfa9af1
Shuvayan Saha <shuvayan@magicmindtechnologies.com>
2017-02-24 13:01:38 +0530

    ver: 1.0.0

--- MenuBar/ViewController.swift
+++ MenuBar/ViewController.swift
@@ -10,15 +10,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var navBar: UINavigationBar!
+    @IBOutlet weak var menuBar: UIView!
+    @IBOutlet weak var lead: NSLayoutConstraint!
+    @IBOutlet weak var trail: NSLayoutConstraint!
+   
+    var menuShowing1 = false
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.navBar.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: 64)
+        
+        lead.constant = -(menuBar.frame.size.width)
+        trail.constant = view.frame.size.width
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   
+
+
+    @IBAction func barButton(_ sender: UIBarButtonItem) {
+   
+        menuShow()
+
     }
+
+
+
+    
+    // Menu Showing Function
+    
+    func menuShow() {
+        
+        if menuShowing1 {
+
+            lead.constant = -(menuBar.frame.size.width)
+            trail.constant = view.frame.size.width
+            
+            UIView.animate(withDuration: 0.3, animations: {
+                
+                self.view.layoutIfNeeded()
+                
+            })
+            
+        } else {
+            
+            lead.constant = 0
+            trail.constant = 80
+
+            UIView.animate(withDuration: 0.3, animations: {
+                
+                self.view.layoutIfNeeded()
+                
+            })
+        }
+        menuShowing1 = !menuShowing1
+    }
+    
+
+
 
 
 }
